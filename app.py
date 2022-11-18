981bc29c3779e3f88b3f34676fa7ecd39f65658d
ahaffg <angharad-fflur@live.co.uk>
2022-11-18 15:18:27 +0000

    attempting to link to mongoDB

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import (Flask, flash, render_template, 
-redirect, session, url_for) 
+from flask import (
+    Flask, flash, render_template, 
+    redirect, session, url_for) 
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -12,7 +13,7 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key  = os.environ.get("SECRET_KEY")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 mongo = PyMongo(app)
@@ -21,8 +22,11 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_opportunities")
 def get_opportunities():
-    opportunities = mongo.gofaluAmGeredigionDB.opportunities.find()
-    return render_template("opportunities.html", opportunities=opportunities)
+    opportunities = list(mongo.db.opportunities.find())
+    opportunity = mongo.db.opportunities.find_one()
+    print(opportunities)
+    print(opportunity)
+    return render_template("opportunities.html", opportunities=opportunities, opportunity=opportunity)
 
 
 if __name__ == "__main__":
